3c1443c480461a1c18ed083c927600a37b539e88
Mysha Sarin <35870219+hayat16@users.noreply.github.com>
2024-06-28 03:16:49 +0600

    Create 49. Group Anagrams.cpp

---
+++ 49. Group Anagrams.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<string> > groupAnagrams(vector<string> strs)
+{
+    map<string,vector<string> >mp;
+    string s;
+    for(int i=0; i<strs.size(); i++)
+    {
+        s=strs[i];
+        sort(s.begin(),s.end());
+        mp[s].push_back(strs[i]);
+    }
+
+    map<string, vector<string> >::iterator it;
+    vector<vector<string> >result;//2D vector
+
+    for (it = mp.begin(); it != mp.end(); it++)
+    {
+        result.push_back(it->second);
+    }
+    return result;
+}
+int main()
+{
+    int n;
+    string s;
+    vector<string>v;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>s;
+        v.push_back(s);
+    }
+    vector<vector<string> >result=groupAnagrams(v);
+
+    for(int i=0; i<result.size(); i++)
+    {
+        for(int j=0; j<result[i].size(); j++)
+        {
+            cout<<result[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
